52fe8740ebd26d0e7f19f86ad7b0e21dadd4eadf
Khairul Anshar <halo@khairulanshar.com>
2022-09-05 11:13:25 +0800

    proxy

--- src/services/starlink.ts
+++ src/services/starlink.ts
@@ -3,7 +3,7 @@ import { Services, Hooks, Reducers } from "@khairul/api"
 
 export const getStarlink = (id) => {
     const hooks = Hooks.useHooks()
-    Services.get(`https://api.spacexdata.com/v4/starlink/${id}`)
+    Services.get(`/v4/starlink/${id}`)
         .then((res) => {
             console.log(res)
             //hooks.getDispatch()({ type: Reducers.ActionType.SET_DATA, data: res.data })
